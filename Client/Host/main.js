2f7bd3a2e2e17cc842b232dd8b6ac393801f376c
Glitch752 <50056537+Glitch752@users.noreply.github.com>
2022-03-19 14:56:05 -0500

    Fixed other websockets set for testing

--- Client/Host/main.js
+++ Client/Host/main.js
@@ -1,5 +1,5 @@
-// var wsc = new WebSocket((window.location.protocol === 'https:' ? 'wss://' : 'ws://') + window.location.host + '/ws');
-var wsc = new WebSocket((window.location.protocol === 'https:' ? 'wss://' : 'ws://') + window.location.host.split(':')[0] + ':6790');
+var wsc = new WebSocket((window.location.protocol === 'https:' ? 'wss://' : 'ws://') + window.location.host + '/ws');
+// var wsc = new WebSocket((window.location.protocol === 'https:' ? 'wss://' : 'ws://') + window.location.host.split(':')[0] + ':6790');
 //The .split(':')[0] is to remove the port number from the hostname.
 
 wsc.onopen = function() {
